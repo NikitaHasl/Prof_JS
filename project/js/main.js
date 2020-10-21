b3b587e256567191e3352859ed91908f73b62dc2
Nikita <khodosov97@mail.ru>
2020-10-21 15:40:38 +0300

    upgrade project

--- project/js/main.js
+++ project/js/main.js
@@ -1,21 +1,55 @@
-const products = [
-  {id: 1, title: 'Notebook', price: 20000},
-  {id: 2, title: 'Mouse', price: 1500},
-  {id: 3, title: 'Keyboard', price: 5000},
-  {id: 4, title: 'Gamepad', price: 4500},
-];
+class ProductList {
+  #goods;
 
-const renderProduct = (title, price, img = 'https://placeimg.com/200/200') => {
-return `<div class="product-item">
-<img src="${img}" alt="${title}">
-<h3>${title}</h3>
-<p>${price}</p>
-<button class = "by-btn">Добавить в корзину.</button>
-</div>`;
-};
+  constructor(container = '.products') {
+    this.container = container;
+    this.#goods = [];
+    this._allProducts = [];
 
-const renderProducts = (list) => {
-  const productList = list.map((item) => renderProduct(item.title, item.price));
-  document.querySelector('.products').innerHTML = productList.join('');
-};
-renderProducts(products);
+    this._fetchGoods();
+    this.#render();
+  }
+
+  _fetchGoods() {
+    this.#goods = [
+      {id: 1, title: 'Notebook', price: 20000},
+      {id: 2, title: 'Mouse', price: 1500},
+      {id: 3, title: 'Keyboard', price: 5000},
+      {id: 4, title: 'Gamepad', price: 4500},
+    ];
+  }
+
+  #render() {
+    const block = document.querySelector(this.container);
+
+    for (let product of this.#goods) {
+      const productObject = new ProductItem(product);
+
+      this._allProducts.push(productObject);
+
+      block.insertAdjacentHTML('beforeend', productObject.getGoodHTML());
+    }
+  }
+}
+
+class ProductItem {
+  constructor(product, img='https://placehold.it/200x150') {
+    this.title = product.title;
+    this.price = product.price;
+    this.id = product.id;
+    this.img = img;
+  }
+
+  getGoodHTML() {
+    return `<div class="product-item" data-id="${this.id}">
+              <img src="${this.img}" alt="Some img">
+              <div class="desc">
+                  <h3>${this.title}</h3>
+                  <p>${this.price} \u20bd</p>
+                  <button class="buy-btn">Купить</button>
+              </div>
+            </div>`;
+  }
+}
+
+const list = new ProductList();
